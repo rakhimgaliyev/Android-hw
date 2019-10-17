23dfed2721839ce1b9914a5c22e000ee989428a8
boss <rakhimgaliev56@gmail.com>
2019-10-17 22:50:49 +0300

    deleted 'save fragment' from main activity

--- app/src/main/java/com/example/hw_1/MainActivity.java
+++ app/src/main/java/com/example/hw_1/MainActivity.java
@@ -7,8 +7,6 @@ import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private final static String FRAGMENT1_KEY = "fragment1";
-
     Fragment fragment1;
 
     @Override
@@ -18,15 +16,7 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState == null) {
             fragment1 = new Fragment1();
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_placeholder, fragment1).commit();
-        } else {
-            fragment1 = getSupportFragmentManager().getFragment(savedInstanceState, FRAGMENT1_KEY);
         }
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        getSupportFragmentManager().putFragment(outState, FRAGMENT1_KEY, fragment1);
     }
 }
 
